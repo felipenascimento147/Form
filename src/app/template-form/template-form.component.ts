552d508bc4d3865b24961926e7cd4f78cefd0bae
Felipe Rodrigues <felipenascimento147@hotmail.com>
2019-01-09 15:44:48 -0200

    Versao final

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -7,13 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(form){
+  onSubmit(form) {
     console.log(form);
+  }
+
+  consultaCEP(cep, form) {
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .subscribe(dados => this.populaDadosForm(dados, form));
+      }
+    }
+  }
+
+  populaDadosForm(dados, form) {
+    var end = JSON.parse(dados._body);
+
+    form.setValue({
+      nome: form.value.nome,
+      email: form.value.email,
+      cep: end.cep,
+      rua: end.logradouro,
+      numero: '',
+      complemento: end.complemento,
+      bairro: end.bairro,
+      cidade: end.localidade,
+      estado: end.uf
+
+    });
   }
 
 }
